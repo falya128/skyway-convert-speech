5cb9265d9fb6ba5a880821c3a6c03739884b811d
falya128 <falya128@users.noreply.github.com>
2023-07-06 21:04:25 +0900

    文字起こしおよび音声変換機能の作成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,35 +1,59 @@
 <script setup>
 import { onMounted, ref } from 'vue'
-import { SkyWayContext, SkyWayChannel, SkyWayStreamFactory } from '@skyway-sdk/core'
+import {
+  SkyWayContext,
+  SkyWayChannel,
+  SkyWayStreamFactory,
+  LocalAudioStream
+} from '@skyway-sdk/core'
 import skyway from '@/utils/skyway'
+import axios from 'axios'
+
+const tokenString = skyway.generateTokenString()
+const video = await SkyWayStreamFactory.createCameraVideoStream()
+const data = await SkyWayStreamFactory.createDataStream()
+const context = await SkyWayContext.Create(tokenString)
+const channel = await SkyWayChannel.FindOrCreate(context, {
+  type: 'p2p',
+  name: 'skyway-room-name'
+})
 
 const isJoined = ref(false)
-const tokenString = skyway.generateTokenString()
-const { audio, video } = await SkyWayStreamFactory.createMicrophoneAudioAndCameraStream()
 
+const messages = ref([])
+const MAX_MESSAGE_LENGTH = 3
+const setMesssage = (data) => {
+  messages.value.push(data)
+  if (messages.value.length > MAX_MESSAGE_LENGTH) {
+    messages.value.shift()
+  }
+}
+
+let member
 const join = async () => {
-  const context = await SkyWayContext.Create(tokenString)
-  const channel = await SkyWayChannel.FindOrCreate(context, {
-    type: 'p2p',
-    name: 'skyway-room-name'
-  })
-  const member = await channel.join()
-  await member.publish(audio)
+  member = await channel.join()
   await member.publish(video)
+  await member.publish(data)
 
   const subscribeAndAttach = async (publication) => {
     if (publication.publisher.id === member.id) return
 
     const { stream } = await member.subscribe(publication.id)
-    switch (stream.track.kind) {
-      case 'video':
-        stream.attach(document.getElementById('remote-video'))
-        break
-      case 'audio':
-        stream.attach(document.getElementById('remote-audio'))
-        break
-      default:
-        return
+    if (stream.contentType === 'data') {
+      stream.onData.add((data) => {
+        setMesssage(data)
+      })
+    } else {
+      switch (stream.track.kind) {
+        case 'video':
+          stream.attach(document.getElementById('remote-video'))
+          break
+        case 'audio':
+          stream.attach(document.getElementById('remote-audio'))
+          break
+        default:
+          return
+      }
     }
   }
   channel.publications.forEach(subscribeAndAttach)
@@ -39,7 +63,46 @@ const join = async () => {
   isJoined.value = true
 }
 
+const fetchAudio = async (text) => {
+  const response = await axios.get(import.meta.env.VITE_FETCH_AUDIO_URL, {
+    params: { text }
+  })
+  const base64Str = response.data
+  const raw = atob(base64Str)
+  return Uint8Array.from(Array.prototype.map.call(raw, (x) => x.charCodeAt(0)))
+}
+
+let audioPublication
 onMounted(async () => {
+  const SpeechRecognition = window.webkitSpeechRecognition || window.SpeechRecognition
+  const recognition = new SpeechRecognition()
+  recognition.continuous = true
+  recognition.lang = 'ja-JP'
+  recognition.onresult = async (event) => {
+    const text = event.results[event.resultIndex][0].transcript
+    console.log({ text })
+    data.write(text)
+
+    const uint8Array = await fetchAudio(text)
+    const audioContext = new AudioContext()
+    const audioBuffer = await audioContext.decodeAudioData(uint8Array.buffer)
+    const source = audioContext.createBufferSource()
+    const mediaStreamDestination = audioContext.createMediaStreamDestination()
+    source.buffer = audioBuffer
+    source.connect(mediaStreamDestination)
+    const { stream } = mediaStreamDestination
+    const tracks = stream.getAudioTracks()
+    const localAudioStream = new LocalAudioStream(tracks[0])
+    if (audioPublication) {
+      await member.unpublish(audioPublication.id)
+    }
+    audioPublication = await member.publish(localAudioStream)
+    setTimeout(() => {
+      source.start()
+    }, 1000)
+  }
+  recognition.start()
+
   const localVideo = document.getElementById('local-video')
   video.attach(localVideo)
   await localVideo.play()
@@ -47,22 +110,29 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div class="py-5 md:py-10 text-center bg-white">
+  <div class="mt-10 text-center bg-white">
     <div class="flex items-center justify-center gap-x-6 flex-col md:flex-row">
-      <video id="local-video" playsinline autoplay muted class="bg-black w-4/5 md:w-2/5" />
+      <div class="w-4/5 md:w-2/5">
+        <video id="local-video" playsinline autoplay muted class="bg-black" />
+      </div>
       <div class="mt-4 md:mt-0 w-4/5 md:w-2/5">
-        <video id="remote-video" playsinline autoplay class="bg-black w-full" />
-        <audio id="remote-audio" playsinline autoplay muted controls class="mt-4" />
+        <template v-if="isJoined">
+          <video id="remote-video" playsinline autoplay muted class="bg-black" />
+          <audio id="remote-audio" playsinline autoplay muted controls class="mt-4" />
+        </template>
+        <button
+          v-else
+          class="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-10"
+          @click="join()"
+        >
+          参加する
+        </button>
       </div>
     </div>
-
-    <button
-      :class="{ 'opacity-50 cursor-not-allowed': isJoined }"
-      :disabled="isJoined"
-      class="bg-blue-500 text-white font-bold py-2 px-4 rounded mt-10"
-      @click="join()"
-    >
-      参加する
-    </button>
+    <div class="m-6">
+      <div v-for="(message, index) in messages.slice().reverse()" :key="index">
+        {{ message }}
+      </div>
+    </div>
   </div>
 </template>
